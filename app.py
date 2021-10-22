16e8d31a58e53a14916432f2eac2d7746bf09936
Michael <haraebm@gmail.com>
2021-10-22 14:36:49 -0300

    Buttons update

--- app.py
+++ app.py
@@ -1,44 +1,46 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-import psycopg2 #pip install psycopg2 
+import psycopg2 
 import psycopg2.extras
+from datetime import datetime
  
 
 app = Flask(__name__)
 
-app.secret_key = "petos_flask_app"
+app.secret_key = "ghwtsgshbj2s2h2bdsweghd"
  
-DB_HOST = "localhost"
-DB_NAME = "empresa_abc"
-DB_USER = "postgres"
-DB_PASS = "tempo290"
+HOST = "localhost"
+DATABASE = "empresa_abc"
+USER = "postgres"
+PASSWORD = "tempo290"
 
 
 # Conectando-se ao banco de dados
-conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+connection = psycopg2.connect(dbname=DATABASE, user=USER, password=PASSWORD, host=HOST)
 
 
 @app.route('/')
 def Index():
 
     # criando o cursor
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
  
     # Selecione todos os dados das duas tabelas usando INNER_JOIN e exiba-os na página.
     user = """SELECT funcionarios.id, funcionarios.nome, funcionarios.sobre_nome, funcionarios.sexo, funcionarios.matricula, 
             tabela_2.senha, tabela_2.salario, tabela_2.cargo, tabela_2.codigo_do_cargo, tabela_2.status_do_colaborador
             FROM funcionarios INNER JOIN tabela_2 ON funcionarios.id = tabela_2.id_funcionario;
             """
-    cur.execute(user) # Execute the SQL
-    list_users = cur.fetchall()
+    cursor.execute(user) # Execute the SQL
+    list_users = cursor.fetchall()
 
     return render_template('index.html', list_users = list_users)
+   
 
  
 @app.route('/add_employee', methods=['POST'])
 def add_employee():
     # Adicionar o funcionario
 
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
     if request.method == 'POST':
         if  len(request.form['nome']) != 0 and len(request.form['sobre_nome']) != 0 and len(request.form['sexo']) != 0 and len(request.form['salario']) != 0:
             if request.form.get("funcionario"):
@@ -56,15 +58,14 @@ def add_employee():
                 status_do_colaborador = request.form['status_do_colaborador']
                 
                 # Insira dados na tabela dos funcionarios
-                cur.execute("INSERT INTO funcionarios (nome, sobre_nome, sexo) VALUES (%s,%s,%s) RETURNING id", (nome, sobre_nome, sexo))
-                id_funcionario = cur.fetchone()[0]
+                cursor.execute("INSERT INTO funcionarios (nome, sobre_nome, sexo) VALUES (%s,%s,%s) RETURNING id", (nome, sobre_nome, sexo))
+                id_funcionario = cursor.fetchone()[0]
 
-                # Insira dados na tabela_2 baseado no "id" da tabela dos funcionarios
-                cur.execute("INSERT INTO tabela_2 (id_funcionario, senha, salario, cargo, codigo_do_cargo, status_do_colaborador) VALUES (%s, %s, %s, %s, %s, %s)", (id_funcionario, senha, salario, cargo, codigo_do_cargo, status_do_colaborador))
-                conn.commit()
-
+                # Insira dados na tabela_2 baseado no "id" dos funcionarios
+                cursor.execute("INSERT INTO tabela_2 (id_funcionario, senha, salario, cargo, codigo_do_cargo, status_do_colaborador) VALUES (%s, %s, %s, %s, %s, %s)", (id_funcionario, senha, salario, cargo, codigo_do_cargo, status_do_colaborador))
+                connection.commit()
+                
                 flash('Funcionário adicionado com sucesso')
-
 
                 return redirect(url_for('Index'))
 
@@ -72,7 +73,7 @@ def add_employee():
                 if  len(request.form['nome']) != 0 and len(request.form['sobre_nome']) != 0 and len(request.form['sexo']) != 0 and len(request.form['salario']) != 0:
                     
                     # Dados do Lider
-                    if request.form.get("liderança"):
+                    if request.form.get("lider"):
 
                         # Tabela dos funcionarios
                         nome = request.form['nome']
@@ -85,13 +86,14 @@ def add_employee():
                         cargo = request.form['cargo']
                         codigo_do_cargo = request.form['codigo_do_cargo']
                         status_do_colaborador = request.form['status_do_colaborador']
-                        
-                        cur.execute("INSERT INTO funcionarios (nome, sobre_nome, sexo) VALUES (%s,%s,%s) RETURNING id", (f"{nome} (Lider-{cargo})", sobre_nome, sexo))
-                        id_funcionario = cur.fetchone()[0]
-                        cur.execute("INSERT INTO tabela_2 (id_funcionario, senha, salario, cargo, codigo_do_cargo, status_do_colaborador) VALUES (%s, %s, %s, %s, %s, %s)", (id_funcionario, senha, salario, cargo, codigo_do_cargo, status_do_colaborador))
-                        conn.commit()
 
-                        flash('Funcionário adicionado com sucesso')
+                        # Insira dados na tabela_2 baseado no "id" dos funcionarios   
+                        cursor.execute("INSERT INTO funcionarios (nome, sobre_nome, sexo) VALUES (%s,%s,%s) RETURNING id", (f"{nome} (Lider-{cargo})", sobre_nome, sexo))
+                        id_funcionario = cursor.fetchone()[0]
+                        cursor.execute("INSERT INTO tabela_2 (id_funcionario, senha, salario, cargo, codigo_do_cargo, status_do_colaborador) VALUES (%s, %s, %s, %s, %s, %s)", (id_funcionario, senha, salario, cargo, codigo_do_cargo, status_do_colaborador))
+                        connection.commit()
+
+                        flash(f'Lider: {nome} {sobre_nome} adicionado com sucesso')
 
                         return redirect(url_for('Index'))
         else:
@@ -105,11 +107,11 @@ def add_employee():
 def get_employee(id):
     # Editar os dados do funcionario
 
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
     sql = "SELECT funcionarios.id, funcionarios.nome, funcionarios.sobre_nome, funcionarios.sexo, tabela_2.senha, tabela_2.cargo, tabela_2.codigo_do_cargo, tabela_2.status_do_colaborador FROM funcionarios INNER JOIN tabela_2 ON funcionarios.id = tabela_2.id_funcionario"
-    cur.execute(f'{sql} WHERE id = {("%s")}', (id))
-    data = cur.fetchall()
-    cur.close()
+    cursor.execute(f'{sql} WHERE id = {("%s")}', (id))
+    data = cursor.fetchall()
+    cursor.close()
     # print(data[0])
 
     return render_template('edit.html', funcionario = data[0])
@@ -117,8 +119,8 @@ def get_employee(id):
 
 @app.route('/update/<id>', methods=['POST'])
 def update_employee(id):
-    # Dados atualizados do funcionario
 
+    # Dados atualizados do funcionario
     if request.method == 'POST':
         nome = request.form['nome']
         sobre_nome = request.form['sobre_nome']
@@ -128,8 +130,8 @@ def update_employee(id):
         codigo_do_cargo = request.form['codigo_do_cargo']
         status_do_colaborador = request.form['status_do_colaborador']
          
-        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute("""
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cursor.execute("""
             UPDATE funcionarios 
             SET nome = %s,
                 sobre_nome = %s,
@@ -137,7 +139,7 @@ def update_employee(id):
             WHERE id = %s """,
             (nome, sobre_nome, sexo, id))
 
-        cur.execute("""
+        cursor.execute("""
             UPDATE tabela_2 
             SET senha = %s,
                 cargo = %s,
@@ -147,7 +149,7 @@ def update_employee(id):
             (senha, cargo, codigo_do_cargo, status_do_colaborador, id))
 
         flash('Dados do funcionário atualizados com sucesso')
-        conn.commit()
+        connection.commit()
 
         return redirect(url_for('Index'))
 
@@ -156,9 +158,9 @@ def update_employee(id):
 def delete_employee(id):
     # Deletar funcionario
 
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute(f'DELETE FROM funcionarios WHERE id = {id}')
-    conn.commit()
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute(f'DELETE FROM funcionarios WHERE id = {id}')
+    connection.commit()
     flash('Funcionário removido com sucesso')
     return redirect(url_for('Index'))
 
@@ -167,7 +169,6 @@ def delete_employee(id):
 def about():
 
     return render_template("about.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
@@ -181,8 +182,12 @@ if __name__ == "__main__":
 #     cursor.execute("ALTER SEQUENCE funcionarios_id_seq RESTART WITH 1")
 #     print("Id reset complete")
  
+# SELECT * FROM funcionarios 
+# INNER JOIN tabela_2 
+# ON funcionarios.id = tabela_2.id_funcionario
 
-# SELECT table1.column1, table2.column2...
-# FROM table1
-# INNER JOIN table2
-# ON table1.common_filed = table2.common_field;
+
+# Delete FROM funcionarios
+# ALTER SEQUENCE funcionarios_id_seq RESTART WITH 1
+
+# SELECT * FROM funcionarios
